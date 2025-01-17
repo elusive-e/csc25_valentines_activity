af588e1e70580c59455e58717297398c33bc64ae
Ellie <ellie.kutyeva@gmail.com>
2025-01-16 17:51:50 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+#code goes here
+# step 1 set up the layout
+#connect buttons
+# mark clearly where the questions and answers are
+#add encryption logic
+#add different encruption methods???
+#add the logic for see your answers and make your own
+#add a fully finished example
+#reaplce textbox with svg

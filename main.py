4c14cfdfd7732d2393c0a7dc139e7ecce848403c
Ellie <ellie.kutyeva@gmail.com>
2025-01-17 09:05:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,40 @@
 #add the logic for see your answers and make your own
 #add a fully finished example
 #reaplce textbox with svg
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit, QVBoxLayout, QDialog, QDialogButtonBox, QMessageBox
+from PyQt5 import QtOpenGL
+from PyQt5 import QtWidgets
+from OpenGL import GLU
+class MainMainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(MainMainWindow, self).__init__()
+        self.setupUi(self)
+        self.mmmm = self.molecule_maker_tab(self)
+        self.mf = self.MenuFunctionsss(self)
+        
+        self.pushButton_14.clicked.connect(self.mmmm.start_bonds)
+        self.pushButton_13.clicked.connect(self.mmmm.start_bonds)
+        self.pushButton_12.clicked.connect(self.mmmm.start_atoms)
+        self.pushButton_11.clicked.connect(self.mmmm.smiles_import)
+        self.pushButton_9.clicked.connect(self.mmmm.load_pdbmmmm)
+        self.pushButton_10.clicked.connect(self.mmmm.save_file)
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainMainWindow()
+    smilesdialog = SMILESInputWindow()
+    bonddialog = BondManager()
+    atomdialog = AtomManager()
+    cswindow = CSInputWindow()
+    cswindow.hide()
+    viewerwindow = Viewer()
+    viewerwindow.hide()
+    smilesdialog.hide()
+    bonddialog.hide()
+    atomdialog.hide()
+    window.show()
+
+
+    sys.exit(app.exec())

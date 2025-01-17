0d301cc77b877e4fad332500310655d1d543ed0e
Ellie <ellie.kutyeva@gmail.com>
2025-01-17 10:52:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,21 @@ class MainMainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
             if self.score >= 2:
                 #match
-                #encrypt a message
+                #Adapted from Ceaser Cipher code by oOperaho
+                alphabet = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+                get_letter, keyword = 0, []
+                #change input code
+                text = str(input("Enter the text: ")).strip().lower()
+                #change the key code 
+                key = int(input("Enter the key: "))
+
+if 0 < key <= 26:
+	for letter in text:
+		get_letter = alphabet.index(letter) + key	
+		keyword.append(alphabet[get_letter])
+    print("".join(keyword))
+else:
+	print("Key must be between 1 and 26!")
 
 
 if __name__ == "__main__":

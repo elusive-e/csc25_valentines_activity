4decf6b44fa81d514cedaf2b1df8515f5173bee7
Ellie <ellie.kutyeva@gmail.com>
2025-02-09 04:35:15 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,96 +1,101 @@
-# mark clearly where the questions and answers are
-#add encryption logic
-#add different encruption methods???
-#add the logic for see your answers and make your own
-#add a fully finished example
-#reaplce textbox with svg
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit, QVBoxLayout, QDialog, QDialogButtonBox, QMessageBox
-from PyQt5 import QtOpenGL
+from PyQt5 import QtCore, QtWidgets
 from PyQt5 import QtWidgets
-from val_end_layout import Ui_end_Screen
-import numpy as np
-import random
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog
 import sys
-import re
-import subprocess
-from val_layout import valwindow
-class MainWindow(QtWidgets.QMainWindow, valwindow):
+#end screen ui
+from val_end_layout import Ui_Dialog
+#main quiz ui
+from val_layout import Ui_Dialog1
+
+class EndDialog(QDialog):
+    #ending dialog intiatied 
     def __init__(self):
-        super(MainWindow, self).__init__()
-        self.score = None
-        self.setupUi(self)
-        self.sk = self.score_keeping(self)
-        #self.mf = self.MenuFunctionsss(self)
-        self.answer_1.clicked.connect(self.sk.subtract_score)
-        self.answer_2.clicked.connect(self.sk.subtract_score)
-        self.submit_button.clicked.connect(self.submit)
-        self.answer_3.clicked.connect(self.sk.subtract_score)
-        self.answer_4.clicked.connect(self.sk.subtract_score)
-        self.answer_5.clicked.connect(self.sk.subtract_score)
-        self.answer_6.clicked.connect(self.sk.subtract_score)
-        self.answer_7.clicked.connect(self.sk.subtract_score)
-        self.answer_8.clicked.connect(self.sk.subtract_score)
-        self.answer_9.clicked.connect(self.sk.subtract_score)
-        self.answer_10.clicked.connect(self.sk.subtract_score)
-        self.answer_11.clicked.connect(self.sk.subtract_score)
-        self.answer_12.clicked.connect(self.sk.subtract_score)
-        self.answer_13.clicked.connect(self.sk.subtract_score)
-        self.answer_14.clicked.connect(self.sk.subtract_score)
-        self.answer_15.clicked.connect(self.sk.subtract_score)
-        self.answer_16.clicked.connect(self.sk.subtract_score)
-        class score_keeping:
-            def __init__(self, MainWindow):
-                self.MainWindow = MainWindow
-                self.score = None
-            def start_score(self):
-                self.score= 0
-                return score
-            def subtract_score(self):
-                self.score -= 1
-                return score
-            def add_score(self):
-                self.score += 1
-                return score
-            def submit(self):
-                end_dialog.show()
-                intro_window.hide()
-                self.match()
-            def show_answers(self):
-                intro_window.show()
-                def match():
-                    if self.score < 2:
-                        pass
-                    
-                        #not a match
-                #add actual use later
-               # self.match_or_not.setText(_translate("Dialog", "We\'re a match!"))
-                #self.message.setText(_translate("Dialog", "Here is your message from your match: \"\""))
-                  #  self.hint.setText(_translate("Dialog", "Hint: Your encryption key is the length of one of the answers from the quiz!"))
-                    
-                    else:
-                        #match
-                        #Adapted from Ceaser Cipher code by oOperaho
-                        alphabet = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
-                        get_letter, keyword = 0, []
-                        #change input code
-                        text = str(input("Enter the text: ")).strip().lower()
-                        #change the key code 
-                        key = int(input("Enter the key: "))
-# 
-#             if 0 < key <= 26:
-#                 for letter in text:
-#                     get_letter = alphabet.index(letter) + key	
-#                     keyword.append(alphabet[get_letter])
-#                 print("".join(keyword))
-#             else:
-#                 print("Key must be between 1 and 26!")
+        # here we make a endialog and apply the ui over it
+        super().__init__()
+        self.ui2 = Ui_Dialog() 
+        self.ui2.setupUi(self)  
+
+class MainMainWindow(QMainWindow):
+    #main window!!
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_Dialog1() 
+        self.ui.setupUi(self)
+        #set up score and ending ui
+        self.score = 0 
+        self.end_dialog = EndDialog()
+
+        #CHANGE YOUR BUTTONS HEREEEEEEEEEEEEEE
+        self.ui.answer_12.clicked.connect(self.submit)
+        self.ui.answer_1.clicked.connect(self.subtract_score)
+        self.ui.answer_2.clicked.connect(self.subtract_score)
+        self.ui.answer_3.clicked.connect(self.add_score)
+        self.ui.answer_4.clicked.connect(self.add_score)
+        self.ui.answer_5.clicked.connect(self.add_score)
+        self.ui.answer_6.clicked.connect(self.add_score)
+        self.ui.answer_7.clicked.connect(self.subtract_score)
+        self.ui.answer_8.clicked.connect(self.subtract_score)
+        self.ui.answer_9.clicked.connect(self.add_score)
+        self.ui.answer_10.clicked.connect(self.subtract_score)
+        self.ui.answer_11.clicked.connect(self.subtract_score)
+        self.ui.answer_13.clicked.connect(self.subtract_score)
+        self.ui.answer_14.clicked.connect(self.add_score)
+        self.ui.answer_15.clicked.connect(self.subtract_score)
+        self.ui.answer_16.clicked.connect(self.add_score)
+
+    def submit(self):
+        #show end dialog lol
+        self.match() 
+        self.end_dialog.show()
+        self.hide() 
+    def add_score(self):
+        #adding score
+        self.score = self.score+1
+    def subtract_score(self):
+        #suvrtracting score
+        self.score = self.score -1
+    def show_answers():
+        #"I was the first, i have seen EVERYTHING"
+        main_window.show()
+    def match(self):
+        score = self.score
+        if score < 2:
+            #call the translate funciton and set the ui
+            #ADDD TEXT HEREEEEEEEEEEE
+            _translate = QtCore.QCoreApplication.translate
+            self.end_dialog.ui2.label.setText(_translate("Dialog", "We\'re not a match!"))
+            self.end_dialog.ui2.label_2.setText(_translate("Dialog", "ADDD TEXT HEREEEEEEEEEEE"))
+        else:
+            # Caesar Cipher logic inspired by oOperaho, likegeeks, and thepythoncode
+            alphabet = "abcdefghijklmnopqrstuvwxyz"
+            keyword = []
+            #gets the name
+            name = self.ui.lineEdit.text().strip() 
+            key = len(name) 
+            message_text = "INSERT YOUR MESSAGE HEREEEEEEEEEEE" 
+
+            if 0 < key <= 26:
+                for letter in message_text:
+                    #encrypts only the laphabet
+                    if letter.isalpha(): 
+                        get_letter = (alphabet.index(letter.lower()) + key) % 26  
+                        keyword.append(alphabet[get_letter])
+                    else: #other stuff added in
+                        keyword.append(letter)  
+
+                encrypted_text = "".join(keyword)
+
+            _translate = QtCore.QCoreApplication.translate    
+            self.end_dialog.ui2.label.setText(_translate("Dialog", "We\'re a match!"))
+            self.end_dialog.ui2.label_2.setText(_translate("Dialog", f"Here is your message from your match: {encrypted_text}"))
+            self.end_dialog.ui2.label_3.setText(_translate("Dialog", "Hint: Your encryption key is the length of one of the answers from the quiz!"))
+   
+
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    intro_window = MainWindow()
-    end_dialog = Ui_end_Screen()
-    intro_window.show()
-
-
-    sys.exit(app.exec())
+    app = QApplication(sys.argv)
+    main_window = MainMainWindow()
+    main_window.show() 
+    end_dialog=EndDialog()
+    end_dialog.hide()
+    sys.exit(app.exec_())
